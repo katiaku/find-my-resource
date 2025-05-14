68f3f19c727301186fd3a9fdbc16c4cbfe3fa03a
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-05-14 20:33:36 +0200

    Seta up frontend scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <h1 className="text-3xl font-bold text-blue-400">Resource Helper</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
