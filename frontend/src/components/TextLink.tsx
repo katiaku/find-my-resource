b06838039b13d0e3eda0beedef85f8f70f513ef1
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-05-15 18:30:53 +0200

    Add TextLink component

---
+++ frontend/src/components/TextLink.tsx
@@ -0,0 +1,18 @@
+type TextLinkProps = {
+  label: string
+  className?: string
+  handleClick: () => void
+}
+
+const TextLink = ({ label, handleClick, className }: TextLinkProps) => {
+  return (
+    <button
+      onClick={handleClick}
+      className={`cursor-pointer px-4 py-2 text-gray-100 transition duration-300 hover:text-gray-300 ${className}`}
+    >
+      <span>{label}</span>
+    </button>
+  )
+}
+
+export default TextLink
