2a8455c1d9dd00e34fcaf98f88a8523e11f8dfdf
Mark <mark.obrien78@btinternet.com>
2025-05-17 10:17:57 +0100

    Add search form component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Header from "./components/Header"
+import SearchForm from "./components/SearchForm"
 
 function App() {
   return (
-    <div className="container">
+    <div className="container mx-auto">
       <Header />
+      <SearchForm />
     </div>
   )
 }
