a44b153ebe8ef933a19d1a8a68c62a6816cd54ea
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-05-18 17:27:45 +0200

    Fix styling in Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,18 @@
 const Header = () => {
   return (
-    <header className="flex items-center flex-col sm:flex-row justify-between xl:justify-evenly p-6">
-      <img className="h-26 w-26 md:h-38 md:w-38 mb-3 sm:mb-0" src="/logo.png" alt="Pictures of various tools that develop your mind"/>
+    <header className="container mx-auto flex flex-col items-center justify-between p-6 sm:flex-row xl:justify-evenly">
+      <img
+        className="mb-3 h-26 w-26 sm:mb-0 md:h-38 md:w-38"
+        src="/logo.png"
+        alt="Pictures of various tools that develop your mind"
+      />
       <div>
-        <h1 className="lg:text-6xl text-4xl text-blue-950 text-center font-semibold">FIND MY RESOURCE</h1>
-        <h2 className="text-base md:text-2xl tracking-widest font-semibold text-center sm:text-right text-amber-500">LEARNING MADE EASY</h2>
+        <h1 className="text-center text-4xl font-semibold text-blue-950 lg:text-6xl">
+          FIND MY RESOURCE
+        </h1>
+        <h2 className="text-center text-base font-semibold tracking-widest text-amber-500 sm:text-right md:text-2xl">
+          LEARNING MADE EASY
+        </h2>
       </div>
     </header>
   )
