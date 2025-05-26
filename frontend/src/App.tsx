caac730b30f7630d1c94c67c4a04c1694c6b6705
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-05-26 12:28:35 +0200

    Remove user state from App to not pass that prop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,24 +4,15 @@ import Layout from "./components/Layout"
 import SearchPage from "./pages/SearchPage"
 import LoginPage from "./pages/LoginPage"
 import DashboardPage from "./pages/DashboardPage"
-import { useUser } from "./context/useUser"
 
 function App() {
-  const { setUser } = useUser()
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<SearchPage />} />
-          <Route
-            path="login"
-            element={<LoginPage page="login" handleSetUserName={setUser} />}
-          />
-          <Route
-            path="signup"
-            element={<LoginPage page="signup" handleSetUserName={setUser} />}
-          />
+          <Route path="login" element={<LoginPage page="login" />} />
+          <Route path="signup" element={<LoginPage page="signup" />} />
           <Route path="dashboard" element={<DashboardPage />} />
         </Route>
       </Routes>
