a092df7638cd8d3b93ef446222960439077e2b41
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-05-26 12:30:48 +0200

    Refactor LoginPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,13 +1,9 @@
 import LoginForm from "../components/LoginForm"
 import SignupForm from "../components/SignupForm"
-import type { LoginPageProps } from "../types/index"
+import type { LoginPageProps } from "../types"
 
-const LoginPage = ({ page, handleSetUserName }: LoginPageProps) => {
-  return page === "login" ? (
-    <LoginForm />
-  ) : (
-    <SignupForm handleSetUserName={handleSetUserName} />
-  )
+const LoginPage = ({ page }: LoginPageProps) => {
+  return page === "login" ? <LoginForm /> : <SignupForm />
 }
 
 export default LoginPage
