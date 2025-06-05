4d0578af261cb946e3c15e5084eae699d9825180
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-06-05 11:10:28 +0200

    Implement cookies

--- frontend/src/context/authContext.tsx
+++ frontend/src/context/authContext.tsx
@@ -1,47 +1,51 @@
 import { useState, useEffect, type ReactNode } from "react"
 import { AuthContext } from "./authContextObject"
 import type { User } from "../types"
+import { baseUrl } from "../api/api"
 
 export interface AuthContextType {
   user: User | null
-  token: string | null
   setUser: (user: User) => void
   clearUser: () => void
 }
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUserState] = useState<User | null>(null)
-  const [token, setToken] = useState<string | null>(null)
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user")
-    const storedToken = localStorage.getItem("token")
-    if (storedUser) {
-      setUserState(JSON.parse(storedUser))
+    const checkAuth = async () => {
+      try {
+        const res = await fetch(`${baseUrl}/auth/check-auth`, {
+          credentials: "include",
+        })
+
+        if (res.ok) {
+          const data = await res.json()
+          setUserState(data.user)
+        } else {
+          setUserState(null)
+        }
+      } catch (error) {
+        console.error("Auth checking error:", error)
+        setUserState(null)
+      }
     }
-    if (storedToken) {
-      setToken(storedToken)
-    }
+
+    checkAuth()
   }, [])
 
-  const setUser = (user: User, authToken?: string) => {
-    setUserState(user)
-    localStorage.setItem("user", JSON.stringify(user))
-    if (authToken) {
-      setToken(authToken)
-      localStorage.setItem("token", authToken)
-    }
-  }
+  const setUser = (user: User) => setUserState(user)
 
   const clearUser = () => {
     setUserState(null)
-    setToken(null)
-    localStorage.removeItem("user")
-    localStorage.removeItem("token")
+    fetch(`${baseUrl}/logout/`, {
+      method: "POST",
+      credentials: "include",
+    }).catch(console.error)
   }
 
   return (
-    <AuthContext.Provider value={{ user, setUser, clearUser, token }}>
+    <AuthContext.Provider value={{ user, setUser, clearUser }}>
       {children}
     </AuthContext.Provider>
   )
